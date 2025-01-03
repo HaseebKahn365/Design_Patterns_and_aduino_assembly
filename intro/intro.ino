e3e11e06e4e88280fe7c0ed34a3e6204c23b3862
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2025-01-03 10:12:08 +0500

    first program

--- intro/intro.ino
+++ intro/intro.ino
@@ -1,10 +1,21 @@
+//this is primarily targetting the assembly
+
+extern "C"
+{
+  void start();
+  void led(byte);
+}
+
 void setup(){
-  
+  start();
   
   }
 
 
   void loop(){
-    
+    led(1);
+    delay(200);
+    led(0);
+    delay(200);
     
     }
