93668155b7909140e8cb835f665ee60d71cf6cd3
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2025-01-10 11:12:25 +0500

    working stuff

--- timer0 programming/1timer0squarewaveinC/timer0sw.ino
+++ timer0 programming/1timer0squarewaveinC/timer0sw.ino
@@ -14,9 +14,9 @@ int main(){
 
 void T0Delay(){
     TCNT0 = 0X00;    // Added missing semicolon
-    TCCR0B = 0x05;   // Changed TCCR0 to TCCR0B USED TO SELECT THE TIMER CLOCK/1024
+    TCCR0 = 0x05;   // Changed TCCR0 SELECT THE TIMER CLOCK/1024
     while((TIFR0 & (1<<TOV0)) == 0); // Changed TIFR to TIFR0
     //STOP THE TIMER
-    TCCR0B = 0x00;   // Changed TCCR0 to TCCR0B
+    TCCR0 = 0x00;   
     TIFR0 = 1<<TOV0; // Changed TIFR to TIFR0
 }
